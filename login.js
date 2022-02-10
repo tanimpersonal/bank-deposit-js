f23cd48d4c98361b49ebe32dac0b6f582772c809
tanimpersonal <tashintanim2019@gmail.com>
2022-02-10 18:24:34 +0600

    first commit

---
+++ login.js
@@ -0,0 +1,17 @@
+const loginButton= document.getElementById('login-button');
+const passwordField= document.getElementById('input-password');
+const emailField= document.getElementById('input-email');
+loginButton.addEventListener('click',function(event){
+    let passwordValue= passwordField.value;
+    let emailValue= emailField.value;
+    if(emailValue=='tanim@gmail.com'&&passwordValue=='tashin'){
+        window.location.href= 'https://www.google.com';
+    } else if(emailValue=='' || passwordValue=='tashin'){
+        alert('Please enter valid email')
+    } else if(emailValue=='tanim@gmail.com' || passwordValue=='' ){
+        alert('Please enter valid password')
+    } else{
+        alert('Please enter valid mail and password');
+    }
+    emailField.value='';
+})
